36a269145325826c5e934bf85d487f9f9e6cc7a7
AnandP201 <pie1002anand@gmail.com>
2021-06-26 09:19:08 +0530

    finished starred page

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -18,7 +18,6 @@ function showsReducer(prevState, action) {
 function usePersistedReducer(reducer, initialState, key) {
   const [state, dispatch] = useReducer(reducer, initialState, initial => {
     const persisted = localStorage.getItem(key);
-
     return persisted ? JSON.parse(persisted) : initial;
   });
 
