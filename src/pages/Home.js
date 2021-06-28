9e9a556c5e77b7f17f4ee607ce8049c1b381fe84
AnandP201 <pie1002anand@gmail.com>
2021-06-28 09:04:12 +0530

    Optimized react components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import MainPageLayout from '../Components/MainPageLayout';
 import { apiGet } from '../misc/config';
 import ShowGrid from '../Components/show/ShowGrid';
@@ -12,6 +12,22 @@ import {
 } from './Home.styled';
 import CustomRadio from '../Components/CustomRadio';
 
+const renderResults = results => {
+  if (results && results.length === 0) {
+    return <div>No results</div>;
+  }
+
+  if (results && results.length > 0) {
+    return results[0].show ? (
+      <ShowGrid data={results} />
+    ) : (
+      <ActorGrid data={results} />
+    );
+  }
+
+  return null;
+};
+
 const Home = () => {
   // eslint-disable-next-line no-unused-vars
 
@@ -21,9 +37,12 @@ const Home = () => {
 
   const isShowsSearch = searchOption === 'shows';
 
-  const onInputChange = ev => {
-    setInput(ev.target.value);
-  };
+  const onInputChange = useCallback(
+    ev => {
+      setInput(ev.target.value);
+    },
+    [setInput]
+  );
 
   const onSearch = () => {
     apiGet(`/search/${searchOption}?q=${input}`).then(result => {
@@ -36,25 +55,10 @@ const Home = () => {
     }
   };
 
-  const onRadioChange = ev => {
+  const onRadioChange = useCallback(ev => {
     setSearchOption(ev.target.value);
-  };
+  }, []);
 
-  const renderResults = () => {
-    if (results && results.length === 0) {
-      return <div>No results</div>;
-    }
-
-    if (results && results.length > 0) {
-      return results[0].show ? (
-        <ShowGrid data={results} />
-      ) : (
-        <ActorGrid data={results} />
-      );
-    }
-
-    return null;
-  };
   return (
     <MainPageLayout>
       <SearchInput
@@ -91,7 +95,7 @@ const Home = () => {
           Search
         </button>
       </SearchButtonWrapper>
-      {renderResults()}
+      {renderResults(results)}
     </MainPageLayout>
   );
 };
